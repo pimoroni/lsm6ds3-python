f202e1bf39c88feadc67ce36cbae241402839416
Phil Howard <github@gadgetoid.com>
2026-08-26 19:47:44 +0100

    Widen ruff ruleset and fix findings

--- examples/read_all.py
+++ examples/read_all.py
@@ -6,5 +6,5 @@ lsm = LSM6DS3()
 
 while True:
     ax, ay, az, gx, gy, gz = lsm.get_readings()
-    print("Accelerometer\nX:{}, Y:{}, Z:{}\nGyro\nX:{}, Y:{}, Z{}\n\n ".format(ax, ay, az, gx, gy, gz))
+    print(f"Accelerometer\nX:{ax}, Y:{ay}, Z:{az}\nGyro\nX:{gx}, Y:{gy}, Z{gz}\n\n ")
     time.sleep(1.0)
